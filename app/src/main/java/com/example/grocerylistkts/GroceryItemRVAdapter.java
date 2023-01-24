4fde4c5cb0f52396bf13c6d86f0df537d3b4accf
Cristian <cgt4@uic.edu>
2023-01-24 04:42:24 -0600

    RecyclerView working

--- app/src/main/java/com/example/grocerylistkts/GroceryItemRVAdapter.java
+++ app/src/main/java/com/example/grocerylistkts/GroceryItemRVAdapter.java
@@ -1,4 +1,57 @@
 package com.example.grocerylistkts;
 
-public class GroceryItemRVAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GroceryItemRVAdapter extends RecyclerView.Adapter<GroceryItemRVAdapter.MyViewHolder>{
+    Context context;
+    ArrayList<GroceryItem> groceryItemArrayList;
+
+    public GroceryItemRVAdapter(Context context, ArrayList<GroceryItem> groceryItemArrayList) {
+        this.context = context;
+        this.groceryItemArrayList = groceryItemArrayList;
+    }
+    @NonNull @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.grocery_recycler_list_item, parent, false);
+        return new GroceryItemRVAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GroceryItemRVAdapter.MyViewHolder holder, int position) {
+        holder.groceryItemCountTextView.setText(groceryItemArrayList.get(position).getItemCount()+"");
+        holder.groceryItemNameTextView.setText(groceryItemArrayList.get(position).getItemName());
+        holder.groceryItemImageView.setImageResource(R.drawable.ic_dashboard_black_24dp);
+        //assign values
+    }
+
+    @Override
+    public int getItemCount() {
+        return groceryItemArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView groceryItemImageView;
+        TextView groceryItemNameTextView;
+        TextView groceryItemCountTextView;
+        public MyViewHolder(@NonNull View itemView) {
+            //Grab views from RVlayout file
+            super(itemView);
+            groceryItemImageView = itemView.findViewById(R.id.imageViewGroceryItem);
+            groceryItemNameTextView = itemView.findViewById(R.id.textViewGroceryName);
+            groceryItemCountTextView = itemView.findViewById(R.id.textViewGroceryCount);
+
+        }
+    }
 }
--- app/src/main/java/com/example/grocerylistkts/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/grocerylistkts/ui/home/HomeFragment.java
@@ -9,8 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.grocerylistkts.GroceryItem;
+import com.example.grocerylistkts.GroceryItemRVAdapter;
+import com.example.grocerylistkts.R;
 import com.example.grocerylistkts.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
@@ -18,31 +22,34 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     ArrayList<GroceryItem> groceryItemArrayList = new ArrayList<>();
+    RecyclerView rv;
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
+        //final TextView textView = binding.textHome;
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        rv = root.findViewById(R.id.groceryItemRecyclerView);
         setUpGroceryItemArrayList();
-
+        GroceryItemRVAdapter adapter = new GroceryItemRVAdapter(this.getActivity(), groceryItemArrayList);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         return root;
     }
+
 
     private void setUpGroceryItemArrayList() {
         String[] groceryItemIDList = new String[]{"first", "second", "third"};
         String[] groceryItemCountList = new String[]{"1", "2", "3"};
         String[] groceryItemNameList = new String[]{"firstName", "secondName", "thirdName"};
 
-        for(int i = 0 ; i < groceryItemArrayList.size(); i++) {
+        for(int i = 0 ; i < groceryItemIDList.length; i++) {
             groceryItemArrayList.add(new GroceryItem(Integer.parseInt(groceryItemCountList[i]), groceryItemNameList[i], groceryItemIDList[i]));
         }
     }
